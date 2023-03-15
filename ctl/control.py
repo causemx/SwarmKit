ba7e22600fea3bf0d67674e49d55495501971d12
Nanji Owl <allen.cause@gmail.com>
2023-03-15 18:02:55 +0800

    Show mode in input payload now.

--- ctl/control.py
+++ ctl/control.py
@@ -1,4 +1,7 @@
+import argparse
 import time
+from enum import Enum
+from threading import Thread
 from pymavlink import mavutil
 
 
@@ -8,26 +11,31 @@ class ControlError(Exception):
     pass
 
 
-def connect(args, retry=3) -> str:
+class ConnectionType(Enum):
+    serial = 1
+    udp = 2 
+
+
+def connect(conn_type, host, port=14550, baud=921600, retry=3) -> str:
     global master
     # _args = args.connect_info
     _retry = 0
-    connect_info = args.ci
     while True:
         try:
             #master = mavutil.mavlink_connection('/dev/serial0', baud=921600)
             #master = mavutil.mavlink_connection('udpin:{}:{}'.format(host, port))
-            if connect_info[0] == "serial":
-                master = mavutil.mavlink_connection(connect_info[1],\
-                                                     baud=int(connect_info[2]))
-            elif connect_info[0] == "udp":
-                master = mavutil.mavlink_connection('udpin:{}'.format(connect_info[1]))
+            if conn_type.value == 1: # serial type
+                master = mavutil.mavlink_connection(host,baud=baud)
+            elif conn_type.value == 2: # udp tpye
+                master = mavutil.mavlink_connection('udpin:{}:{}'.format(host, port))
             else:
                 return 'unknown_connection_type'
 
-            master.wait_heartbeat()
-            print('connected')
-            break
+            m = master.wait_heartbeat()
+            if m:
+                Thread(target=handle_input, args=(master,)).start() 
+
+            return 'connected'
         except ControlError:
             print("retry...:{}".format(_retry))
             _retry = _retry + 1
@@ -35,6 +43,47 @@ def connect(args, retry=3) -> str:
                 print('fail_to_connect_uav')
                 raise ConnectionError
             time.sleep(0.3)
+
+
+def handle_input(master):
+    while True:
+        msg = master.recv_match(type='HEARTBEAT', blocking = True)
+        mode = mavutil.mode_string_v10(msg)
+        ipt = input("{}> ".format(mode))
+        print(ipt)
+    
+
+
+def parse(args):
+    parser = argparse.ArgumentParser()
+    subparser = parser.add_subparsers()
+
+    parser_arm = subparser.add_parser("arm", help="Arming/Disarm UAV throttle.")
+    # parser_arm.add_argument('--isarm', type=int, default=1, help="1: arm, 0: disarm.")
+    parser_arm.set_defaults(func=arm)
+
+    parser_takeoff = subparser.add_parser("takeoff", \
+        help="Takeoff UAV to expect height.")
+    parser_takeoff.add_argument('height', type=int, default=1,\
+                                 help="Expect height for UAV takeoff.")
+    parser_takeoff.set_defaults(func=takeoff)
+
+
+    parser_setmode = subparser.add_parser("setmode", help="Set UAV mode.")
+    parser_setmode.add_argument("mode", type=str, help="Enter expected mode here.")
+    parser_setmode.set_defaults(func=set_mode)
+    
+    parser_move = subparser.add_parser("move", 
+        help="Make UAV moving [North/South, East/West, Down/Up] in local coord.")
+    parser_move.add_argument("movement", type=int, nargs='+', 
+        help="Move [east/west] [north/south] [down/up] for meters")
+    parser_move.set_defaults(func=move)
+
+    args = parser.parse_args(args)
+    args.func(args)
+
+
+
 
 
 def arm(args) -> None:
@@ -114,4 +163,3 @@ def move(args) -> None:
         print(msg)
         break
 
-def 
